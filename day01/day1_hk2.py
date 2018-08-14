e15b7b0308258767b73cb663435bb13c2a15bdf9
Liu Hui <liuhui@ahu.edu.cn>
2018-08-14 13:12:17 +0800

    login api 1st commit

---
+++ day01/day1_hk2.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+import sys
+import os
+
+username = 'alex'
+password = 'alex123'
+locked = 1
+
+retry_counter = 0
+
+#handle the username and passwd empty issue
+while retry_counter < 3:
+        user = input('Username:')
+        if len(user) == 0:
+                print ("Username cannot be empty!")
+                continue
+               
+        passwd = input('Password:')
+        if len(passwd) == 0 :
+                print ("Password cannot be empty!")
+                continue
+
+        
+
+#going to the loging verification part
+
+
+#reading blacklist
+               
+        f = open('blacklist.txt','r')
+        for line in f.readlines():
+             # 使用line.replace('\n','') 替换换行符
+             if line.replace('\n','') == user:
+                  locked = 0
+                  print ('Your username is locked')
+                  sys.exit()
+             else:
+                  continue
+        f.close()
+
+        if user == username and passwd == password:       #means passed the verfication
+             sys.exit ('Welcome %s login to our system!'%user)
+
+        else:
+             retry_counter += 1
+             print ('Wrong username or password, you have %s more chances!'% (3-retry_counter))
+
+             if retry_counter == 3:
+                  f = open('blacklist.txt','a')
+                  f.write('\n')
+                  f.writelines(user)
+                  f.close()
+             else:
+                  continue
